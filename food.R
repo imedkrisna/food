e310dccf8f2cac2c595cc98e0970259c2783062f
imedkrisna <imedkrisna@gmail.com>
2022-11-01 12:22:08 +0700

    update bound F test

--- food.R
+++ food.R
@@ -3,27 +3,41 @@ library("tidyverse")
 library("readxl")
 library("sjPlot")
 
+## Reading data
+
 ibs<-read_excel("ibs.xlsx",sheet="IBS2")
 mikro<-read_excel("mikrokecil.xlsx",sheet="mikro")
 kecil<-read_excel("mikrokecil.xlsx",sheet="kecil")
 pdb<-read_excel("pdb.xlsx",sheet="pdb")
 
-ibs1x<-ardl(ln~lintm,data=ibs,order = c(1,1))|>summary()
-ibs2x<-ardl(lnaker~lintm,data=ibs,order = c(1,1))|>summary()
-ibs3x<-ardl(loutput~lintm,data=ibs,order = c(1,1))|>summary()
-ibs4x<-ardl(lva~lintm,data=ibs,order = c(1,1))|>summary()
-ibs5x<-ardl(ltax~lintm,data=ibs,order = c(1,1))|>summary()
-ibs6x<-ardl(lw~lintm,data=ibs,order = c(1,1))|>summary()
-ibs7x<-ardl(lvan~lintm,data=ibs,order = c(1,1))|>summary()
-ibs8x<-ardl(lvana~lintm,data=ibs,order = c(1,1))|>summary()
+## Running ARDL(1,1)
 
-mikro1x<-ardl(ln~lintm,data=mikro,order = c(1,1))   |>summary()
-mikro2x<-ardl(lnaker~lintm,data=mikro,order = c(1,1)) |>summary()
-mikro3x<-ardl(loutput~lintm,data=mikro,order = c(1,1))|>summary()
-mikro4x<-ardl(lva~lintm,data=mikro,order = c(1,1))|>summary()
-mikro5x<-ardl(lvan~lintm,data=mikro,order = c(1,1))|>summary()
-mikro6x<-ardl(lvana~lintm,data=mikro,order = c(1,1))|>summary()
-mikro7x<-ardl(lw~lintm,data=mikro,order = c(1,1))|>summary()
+ibs1x<-ardl(ln~lintm,data=ibs,order = c(1,1))
+ibs2x<-ardl(lnaker~lintm,data=ibs,order = c(1,1))
+ibs3x<-ardl(loutput~lintm,data=ibs,order = c(1,1))
+ibs4x<-ardl(lva~lintm,data=ibs,order = c(1,1))
+ibs5x<-ardl(ltax~lintm,data=ibs,order = c(1,1))
+ibs6x<-ardl(lw~lintm,data=ibs,order = c(1,1))
+ibs7x<-ardl(lvan~lintm,data=ibs,order = c(1,1))
+ibs8x<-ardl(lvana~lintm,data=ibs,order = c(1,1))
+
+mikro1x<-ardl(ln~lintm,data=mikro,order = c(1,1))
+mikro2x<-ardl(lnaker~lintm,data=mikro,order = c(1,1)) 
+mikro3x<-ardl(loutput~lintm,data=mikro,order = c(1,1))
+mikro4x<-ardl(lva~lintm,data=mikro,order = c(1,1))
+mikro5x<-ardl(lvan~lintm,data=mikro,order = c(1,1))
+mikro6x<-ardl(lvana~lintm,data=mikro,order = c(1,1))
+mikro7x<-ardl(lw~lintm,data=mikro,order = c(1,1))
+
+kecil1x<-ardl(ln~lintm,data=kecil,order = c(1,1))   
+kecil2x<-ardl(lnaker~lintm,data=kecil,order = c(1,1))
+kecil3x<-ardl(loutput~lintm,data=kecil,order = c(1,1))
+kecil4x<-ardl(lva~lintm,data=kecil,order = c(1,1))
+kecil5x<-ardl(lvan~lintm,data=kecil,order = c(1,1))
+kecil6x<-ardl(lvana~lintm,data=kecil,order = c(1,1))
+kecil7x<-ardl(lw~lintm,data=kecil,order = c(1,1))
+
+## Making regression table
 
 tab_model(mikro3x,file="mikro3x.html")
 tab_model(mikro4x,file="mikro4x.html")
@@ -31,20 +45,33 @@ tab_model(mikro5x,file="mikro5x.html")
 tab_model(mikro6x,file="mikro6x.html")
 tab_model(mikro7x,file="mikro7x.html")
 
-kecil1x<-ardl(ln~lintm,data=kecil,order = c(1,1))   |>summary()
-kecil2x<-ardl(lnaker~lintm,data=kecil,order = c(1,1)) |>summary()
-kecil3x<-ardl(loutput~lintm,data=kecil,order = c(1,1))|>summary()
-kecil4x<-ardl(lva~lintm,data=kecil,order = c(1,1))|>summary()
-kecil5x<-ardl(lvan~lintm,data=kecil,order = c(1,1))|>summary()
-kecil6x<-ardl(lvana~lintm,data=kecil,order = c(1,1))|>summary()
-kecil7x<-ardl(lw~lintm,data=kecil,order = c(1,1))|>summary()
-
 tab_model(kecil3x,file="kecil3x.html")
 tab_model(kecil4x,file="kecil4x.html")
 tab_model(kecil5x,file="kecil5x.html")
 tab_model(kecil6x,file="kecil6x.html")
 tab_model(kecil7x,file="kecil7x.html")
 
+## Bound F test table
+
+bm3<-bounds_f_test(mikro3x,case=2)
+bm4<-bounds_f_test(mikro4x,case=2)
+bm5<-bounds_f_test(mikro5x,case=2)
+bm6<-bounds_f_test(mikro6x,case=2)
+bm7<-bounds_f_test(mikro7x,case=2)
+bk3<-bounds_f_test(kecil3x,case=2)
+bk4<-bounds_f_test(kecil4x,case=2)
+bk5<-bounds_f_test(kecil5x,case=2)
+bk6<-bounds_f_test(kecil6x,case=2)
+bk7<-bounds_f_test(kecil7x,case=2)
+
+bm<-tibble(variable=c("log output","log value added","log value added per firm","log value added per person","log average wage"),
+            stat=c(bm3$statistic,bm4$statistic,bm5$statistic,bm6$statistic,bm7$statistic),
+       pval=c(bm3$p.value,bm4$p.value,bm5$p.value,bm6$p.value,bm7$p.value))
+bk<-tibble(variable=c("log output","log value added","log value added per firm","log value added per person","log average wage"),
+           stat=c(bk3$statistic,bk4$statistic,bk5$statistic,bk6$statistic,bk7$statistic),
+           pval=c(bk3$p.value,bk4$p.value,bk5$p.value,bk6$p.value,bk7$p.value))
+tab_df(bm,file="bound_mikro.html")
+tab_df(bk,file="bound_kecil.html")
 #pdb1<-ardl(lmamin~lintm,data=pdb,order=c(1,3))|>summary()
 #pdb2<-ardl(lresto~lintm,data=pdb,order=c(1,3))|>summary()
 
